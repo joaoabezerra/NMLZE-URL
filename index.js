c41dffa7898f151a9ece76702b19fc739bffbc3c
joaoabezerra <gamestooony@outlook.com>
2026-07-23 21:39:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    document.querySelector('.animar-entrada').classList.add('ativa');
+};
+let link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'CSS styles/style1.css';
+document.head.appendChild(link);
+
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader-wrapper');
+    setTimeout(() => {
+        const status = document.querySelector('.glitch-text');
+        if(status) status.innerText = "ACCESS_GRANTED";
+        setTimeout(() => {
+            loader.classList.add('fade-out');
+        }, 600);
+    }, 3000);
+});
+
+document.querySelectorAll('.masonry-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        console.log("REDIRECTION_START: " + this.href);
+    });
+});
